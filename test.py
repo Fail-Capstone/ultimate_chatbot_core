58c8bd3c090e860511cc570d9600bbc4ecb97413
huyan3103 <lehuyan04@gmail.com>
2021-11-18 20:29:15 +0700

    update

---
+++ test.py
@@ -0,0 +1,7 @@
+# from db_connect import get_collection
+# import json
+
+# with open('data.json', encoding='utf8') as file:
+#     data = json.loads(file.read())
+# intents = get_collection('intents')
+# intents.insert_many(data)
